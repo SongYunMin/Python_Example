ef0dbd6c4ee750aaa4a23d4f91dd91d3a03cb141
YunMin-Song <thd930308@naver.com>
2020-10-16 01:27:39 +0900

    Python Example Update

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hi")
+# input(__prompt) : prompt는 있어도 되고 없어도 됨
+# banjang = input("반장의 이름을 입력하세요")
+# print("우리반 반장 이름은", banjang)
+
+# input(__prompt)은 문자열을 입력 받기 때문에 원하는 결과가 출력되지 않음
+number1 = int(input("첫번째 숫자를 입력하세요 : "))
+number2 = int(input("두번째 숫자를 입력하세요 : "))
+
+print(number1 / number2)
+# 나눗셈
+    # 몫          : //
+    # 나머지       : %
+    # 몫 + 나머지  : /
+# 거듭제곱 : ** 밑 ** 승수
